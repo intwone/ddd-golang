f15a039cf9989a083d71e665560bf9f8d2f018c6
intwone <cassiointw1993@gmail.com>
2023-09-16 19:20:25 -0300

    feat: add usecase to comment on answer

---
+++ internal/domain/forum/application/use_cases/comment_on_answer.go
@@ -0,0 +1,46 @@
+package use_cases
+
+import (
+	"github.com/intwone/ddd-golang/internal/domain/forum/application/repositories"
+	"github.com/intwone/ddd-golang/internal/domain/forum/enterprise"
+)
+
+type CommentOnAnswerUseCaseInput struct {
+	AuthorID string
+	AnswerID string
+	Content  string
+}
+
+type CommentOnAnswerUseCaseInterface interface {
+	Execute(input CommentOnAnswerUseCaseInput) (enterprise.QuestionComment, error)
+}
+
+type DefaultCommentOnAnswerUseCase struct {
+	AnswerRepository        repositories.AnswerRepositoryInterface
+	AnswerCommentRepository repositories.AnswerCommentsRepositoryInterface
+}
+
+func NewDefaultCommentOnAnswerUseCase(answerRepository repositories.AnswerRepositoryInterface, answerComentsRepository repositories.AnswerCommentsRepositoryInterface) *DefaultCommentOnAnswerUseCase {
+	return &DefaultCommentOnAnswerUseCase{
+		AnswerRepository:        answerRepository,
+		AnswerCommentRepository: answerComentsRepository,
+	}
+}
+
+func (uc *DefaultCommentOnAnswerUseCase) Execute(input CommentOnAnswerUseCaseInput) (enterprise.AnswerComment, error) {
+	_, err := uc.AnswerRepository.GetByID(input.AnswerID)
+
+	if err != nil {
+		return enterprise.AnswerComment{}, err
+	}
+
+	answerComment := enterprise.NewAnswerComment(input.Content, input.AnswerID)
+
+	createErr := uc.AnswerCommentRepository.Create(answerComment)
+
+	if createErr != nil {
+		return enterprise.AnswerComment{}, createErr
+	}
+
+	return *answerComment, nil
+}
---
+++ internal/domain/forum/application/use_cases/comment_on_answer_test.go
@@ -0,0 +1,59 @@
+package use_cases_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	uc "github.com/intwone/ddd-golang/internal/domain/forum/application/use_cases"
+	"github.com/intwone/ddd-golang/internal/domain/forum/enterprise"
+	mock "github.com/intwone/ddd-golang/internal/test/mocks"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCommentOnAnswerUseCase_Execute(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	t.Run("should create a comment on answer", func(t *testing.T) {
+		answerCommentRepo := mock.NewMockAnswerCommentsRepositoryInterface(ctrl)
+		answerCommentRepo.EXPECT().Create(gomock.Any()).AnyTimes()
+
+		answer := enterprise.NewAnswer("Content Test", "1", "1")
+		answerRepo := mock.NewMockAnswerRepositoryInterface(ctrl)
+		answerRepo.EXPECT().GetByID(gomock.Any()).Return(*answer, nil).AnyTimes()
+
+		useCase := uc.NewDefaultCommentOnAnswerUseCase(answerRepo, answerCommentRepo)
+
+		input := uc.CommentOnAnswerUseCaseInput{
+			AuthorID: "1",
+			AnswerID: "1",
+			Content:  "Content Test",
+		}
+
+		_, err := useCase.Execute(input)
+
+		require.Nil(t, err)
+	})
+
+	t.Run("should not create a comment on answer", func(t *testing.T) {
+		answerCommentRepo := mock.NewMockAnswerCommentsRepositoryInterface(ctrl)
+		answerCommentRepo.EXPECT().Create(gomock.Any()).AnyTimes()
+
+		answer := enterprise.Answer{}
+		answerRepo := mock.NewMockAnswerRepositoryInterface(ctrl)
+		answerRepo.EXPECT().GetByID(gomock.Any()).Return(answer, errors.New("any")).AnyTimes()
+
+		useCase := uc.NewDefaultCommentOnAnswerUseCase(answerRepo, answerCommentRepo)
+
+		input := uc.CommentOnAnswerUseCaseInput{
+			AuthorID: "1",
+			AnswerID: "1",
+			Content:  "Content Test",
+		}
+
+		_, err := useCase.Execute(input)
+
+		require.NotNil(t, err)
+	})
+}
